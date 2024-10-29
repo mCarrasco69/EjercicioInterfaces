5b102b2c6f774375c8680bcdaf48d343603e4dc3
J Miguel MC <162650216+mCarrasco69@users.noreply.github.com>
2024-10-29 09:45:01 -0600

    en el main principal

---
+++ EjercicioInterfaces/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Operaciones operaciones = new Operaciones();
+
+        Console.WriteLine("Ingrese el radio para área, longitud y volumen:");
+        double radio = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ingrese perímetro y superficie:");
+        double lado = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("los paramametros ingresados son :");
+
+        Console.WriteLine($"Área: {operaciones.Area(radio)} Cm");
+        Console.WriteLine($"Longitud: {operaciones.Longitud(radio)}Cm");
+        Console.WriteLine($"Volumen: {operaciones.Volumen(radio)}Cm");
+        Console.WriteLine($"Perímetro: {operaciones.Perimetro(lado)}Cm");
+        Console.WriteLine($"Superficie: {operaciones.Superficie(lado)}Cm");
+
+        operaciones.Imprimir();
+    }
+}
