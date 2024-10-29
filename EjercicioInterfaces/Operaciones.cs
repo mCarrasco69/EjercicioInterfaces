1e0e6f071a25bd213e6efe92ac80c271b43cc1b3
J Miguel MC <162650216+mCarrasco69@users.noreply.github.com>
2024-10-29 09:44:41 -0600

    clase operaciones

---
+++ EjercicioInterfaces/Operaciones.cs
@@ -0,0 +1,40 @@
+﻿using EjercicioInterfaces;
+
+public class Operaciones : IOperaciones
+{
+    public double Area(double radio)
+    {
+        return Math.PI * Math.Pow(radio, 2);
+    }
+
+    public double Longitud(double radio)
+    {
+        return 2 * Math.PI * radio;
+    }
+
+    public double Volumen(double radio)
+    {
+        return (4 / 3) * Math.PI * Math.Pow(radio, 3);
+    }
+
+    public double Perimetro(double lado)
+    {
+        return 4 * lado;
+    }
+
+    public double Superficie(double lado)
+    {
+        return lado * lado;
+    }
+
+    public void Imprimir()
+    {
+        Console.WriteLine();
+        Console.WriteLine("Resultados de las operaciones:");
+        Console.WriteLine($"Área: {Area(5)}Cm");
+        Console.WriteLine($"Longitud: {Longitud(5)}Cm");
+        Console.WriteLine($"Volumen: {Volumen(5)}Cm");
+        Console.WriteLine($"Perímetro: {Perimetro(5)}Cm");
+        Console.WriteLine($"Superficie: {Superficie(5)}Cm");
+    }
+}
