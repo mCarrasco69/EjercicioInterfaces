2f808aa79d2c5e2974176767c86ed47c62dfc925
J Miguel MC <162650216+mCarrasco69@users.noreply.github.com>
2024-10-29 09:44:18 -0600

    implementado interfaz

---
+++ EjercicioInterfaces/IOperaciones.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace EjercicioInterfaces
+{
+    public interface IOperaciones
+    {
+
+        double Area(double radio);
+        double Longitud(double radio);
+        double Volumen(double radio);
+        double Perimetro(double lado);
+        double Superficie(double lado);
+
+
+
+
+
+
+
+    }
+}
